a5453b46aafaf8a715d46c855781b9d3cc5c1fe7
³ʳᵈ <third@0xjester.com>
2025-08-07 12:40:53 +0100

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -24,6 +24,7 @@ namespace MathLib {
      * @param posY1 The Y position of the measure to.
      */
     //% block
+    //% blockId="calcDistance" block="CalcDistance from X:$posX Y:$posY to X:$posX1 Y:$posY1"
     export function calcDistance(posX: number, posY: number, posX1: number, posY1: number): number {
         let xDiff = posX - posX1
         let yDiff = posY - posY1
@@ -31,11 +32,58 @@ namespace MathLib {
     }
 
     /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * TODO: Returns the value of pi.
      */
     //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% blockID="PI" block="PI π"
+    export function PI(): number {
+        return Math.PI
+    }
+
+    /**
+     * TODO: Limit a the given value between given minimal and maximum value.
+     * @param value the value you wish to apply the limit to.
+     * @param minValue the minimal value.
+     * @param maxValue the maximum value.
+     */
+    //% block
+    //% blockId="clamp" block="Clamp Value:$value Min:$minValue Max:$maxValue"
+    export function clamp(value: number, minValue: number, maxValue: number): number {
+        return Math.min(Math.max(value, maxValue), minValue)
+    }
+
+    /**
+     * TODO: Converts the given angle in degrees to radians.
+     * @param degrees The angle in degrees to convert.
+     */
+    //% block
+    //% blockId="clamp" block="ToRadians $degrees"
+    export function toRadians(degrees: number): number {
+        return (degrees * Math.PI) / 180
+    }
+
+    /**
+     * TODO: Converts the given radians to degrees.
+     * @param radians The radians to convert to degrees.
+     */
+    //% block
+    //% blockId="toDegrees" block="toDegrees $radians"
+    export function toDegrees(radians: number): number {
+        return radians * 57.29577951308232
+    }
+
+    /**
+     * TODO: Calculates the angle between position.
+     * @param posX The X position of the measure from.
+     * @param posY The Y position of the measure from.
+     * @param posX1 The X position of the measure to.
+     * @param posY1 The Y position of the measure to.
+    */
+    //% block
+    //% blockId="calcAngle" block="CalcAngle from X:$posX Y:$posY to X:$posX1 Y:$posY1"
+    export function calcAngle(posX: number, posY: number, posX1: number, posY1: number): number {
+        let xDiff = posX - posX1
+        let yDiff = posY - posY1
+        return toDegrees(Math.atan2(yDiff, xDiff)) - 180
     }
 }
