0e36ae0c1cfa151defc85f55cad97d26a8cafe71
³ʳᵈ <third@0xjester.com>
2025-08-07 10:40:00 +0100

    Test for first function

---
+++ custom.ts
@@ -0,0 +1,41 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://arcade.makecode.com/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#990099 icon=""
+namespace MathLib {
+    /**
+     * TODO: Calculates the distance between to two points.
+     * @param posX The X position of the measure from.
+     * @param posY The Y position of the measure from.
+     * @param posX1 The X position of the measure to.
+     * @param posY1 The Y position of the measure to.
+     */
+    //% block
+    export function calcDistance(posX: number, posY: number, posX1: number, posY1: number): number {
+        let xDiff = posX - posX1
+        let yDiff = posY - posY1
+        return Math.sqrt((xDiff * xDiff) - (yDiff * yDiff))
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
